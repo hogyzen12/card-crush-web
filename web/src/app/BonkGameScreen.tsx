63f6bcb0ecaaafa2366611e1f8de0560f8904415
hogyzen12 <billypapas12@gmail.com>
2024-06-03 21:10:45 +0200

    adding audio and custom animations

--- web/src/app/BonkGameScreen.tsx
+++ web/src/app/BonkGameScreen.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { atom, useRecoilState } from "recoil";
 import { Connection, PublicKey } from '@solana/web3.js';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { getWalletBalance } from './utils/wallet';
 import { createBonkTx } from './transactionBonk';
 import axios from 'axios';
-import matchGif from '../assets/match.gif'; // Import the GIF file
 
 const connection = new Connection('https://damp-fabled-panorama.solana-mainnet.quiknode.pro/186133957d30cece76e7cd8b04bce0c5795c164e/');
 
@@ -13,9 +12,10 @@ const deepCopyBoard = (originalBoard: number[][]): number[][] => {
   return originalBoard.map(row => [...row]);
 };
 
-const gridRows = 5;
-const gridCols = 5;
+const gridRows = 6;
+const gridCols = 6;
 const matchGifIndex = 42;
+const initialTurnLimit = 24;
 
 const candyImages = [
   "assets/backpack.png",
@@ -23,9 +23,23 @@ const candyImages = [
   "assets/jules.png",
   "assets/nyla.png",
   "assets/otter.png",
-  "assets/tetsu.png",
-  matchGif
+  "assets/tetsu.png"
 ];
+
+const candyGifs = [
+  "assets/match.gif",
+  "assets/electric.gif",
+  "assets/metame.gif",
+  "assets/match.gif",
+  "assets/electric.gif",
+  "assets/metame.gif"
+];
+
+const activateSound = new Audio("assets/activate.mp3");
+const framexSound = new Audio("assets/framex.mp3");
+const fireSound = new Audio("assets/fire.mp3");
+const backgroundMusic = new Audio("assets/backing.mp3");
+backgroundMusic.loop = true;
 
 let currentSeed = "3oLQ3tFiwrD1w1FXU6j7hmHLyYv5suye5Ek9cCKYomNZHxhiDKprMSb3rU3UKRq9v3HMTmzjMVUg79Y5ygHtffkL";
 
@@ -34,7 +48,7 @@ const generateBoardFromSeed = (currentSeed: string): number[][] => {
   for (let i = 0; i < gridRows; i++) {
     for (let j = 0; j < gridCols; j++) {
       const seedChar = currentSeed[i * gridRows + j];
-      board[i][j] = seedChar.charCodeAt(0) % 6;
+      board[i][j] = seedChar.charCodeAt(0) % candyImages.length;
     }
   }
   return board;
@@ -100,6 +114,11 @@ const animationBoardState = atom({
   default: generateBoardFromSeed(currentSeed),
 });
 
+const turnLimitState = atom({
+  key: 'turnLimitState',
+  default: initialTurnLimit,
+});
+
 export function BonkGameScreen() {
   const { publicKey, signTransaction } = useWallet();
   const [transactionStatus, setTransactionStatus] = useRecoilState(transactionStatusState);
@@ -108,18 +127,31 @@ export function BonkGameScreen() {
   const [board, setBoard] = useRecoilState(boardState);
   const [matchCount, setMatchCount] = useRecoilState(matchCountState);
   const [turnCount, setTurnCount] = useRecoilState(turnCountState);
+  const [turnLimit, setTurnLimit] = useRecoilState(turnLimitState);
   const [selectedTile, setSelectedTile] = useRecoilState(selectedTileState);
   const [moves, setMoves] = useRecoilState(movesState);
   const [balance, setBalance] = useRecoilState(balanceState);
   const [signature, setSignature] = useRecoilState(signatureState);
   const [currentSeed, setCurrentSeed] = useRecoilState(currentSeedState);
   const [animationBoard, setAnimationBoard] = useRecoilState(animationBoardState);
+  const [isMuted, setIsMuted] = useState(false);
+  const [volume, setVolume] = useState(0.5);
 
   useEffect(() => {
     if (publicKey) {
       fetchWalletData();
     }
   }, [publicKey]);
+
+  useEffect(() => {
+    // Play background music
+    if (!isMuted) {
+      backgroundMusic.volume = volume;
+      backgroundMusic.play().catch(error => console.error('Error playing background music:', error));
+    } else {
+      backgroundMusic.pause();
+    }
+  }, [isMuted, volume]);
 
   const fetchWalletData = async () => {
     try {
@@ -133,12 +165,12 @@ export function BonkGameScreen() {
   };
 
   useEffect(() => {
-    if (turnCount >= 23) {
+    if (turnCount >= turnLimit - 1) {
       setShowNotification(true);
     } else {
       setShowNotification(false);
     }
-  }, [turnCount]);
+  }, [turnCount, turnLimit]);
 
   useEffect(() => {
     const fetchCurrentSeed = async () => {
@@ -173,58 +205,98 @@ export function BonkGameScreen() {
     setMatchCount(0);
     setTurnCount(0);
     setMoves([]);
+    setTurnLimit(initialTurnLimit); // Reset turn limit to initial value
   };
 
   const getReplacementIndices = (matchedIndex: number): number[] => {
-    const previousIndex = matchedIndex - 1 < 0 ? candyImages.length - 2 : matchedIndex - 1;
-    const nextIndex = (matchedIndex + 1) % (candyImages.length - 1);
+    const previousIndex = matchedIndex - 1 < 0 ? candyImages.length - 1 : matchedIndex - 1;
+    const nextIndex = (matchedIndex + 1) % candyImages.length;
 
-    return [previousIndex, candyImages.length - 2, nextIndex];
+    return [previousIndex, matchedIndex, nextIndex];
+  };
+
+  const replaceCandies = (mutableBoard: number[][], row: number, col: number, rowInc: number, colInc: number, len: number, matchedType: number) => {
+    let indices = getReplacementIndices(matchedType);
+    mutableBoard[row][col] = indices[0];
+    for (let i = 1; i < len - 1; i++) {
+      mutableBoard[row + i * rowInc][col + i * colInc] = indices[1];
+    }
+    mutableBoard[row + (len - 1) * rowInc][col + (len - 1) * colInc] = indices[2];
+
+    // Apply special rules within the replaceCandies function
+    if (matchedType === 1) {
+      // Special rule for bonk.png (index 1)
+      if (rowInc === 0) {
+        // Match in a row
+        for (let i = 0; i < gridCols; i++) {
+          mutableBoard[row][i] = i;
+        }
+      } else {
+        // Match in a column
+        for (let i = 0; i < gridRows; i++) {
+          mutableBoard[i][col] = i;
+        }
+      }
+    } else if (matchedType === 0) {
+      // Special rule for backpack.png (index 0)
+      setTurnLimit(prevLimit => prevLimit + 1);
+    }
   };
 
   const detectAndReplaceMatches = (newBoard: number[][]) => {
     let matches = 0;
 
-    const replaceCandies = (mutableBoard: number[][], row: number, col: number, rowInc: number, colInc: number, len: number, matchedType: number) => {
-      let indices = getReplacementIndices(matchedType);
-      mutableBoard[row][col] = indices[0];
-      for (let i = 1; i < len - 1; i++) {
-        mutableBoard[row + i * rowInc][col + i * colInc] = indices[1];
-      }
-      mutableBoard[row + (len - 1) * rowInc][col + (len - 1) * colInc] = indices[2];
-    };
+    const matchAndReplace = (row: number, col: number, rowInc: number, colInc: number, len: number) => {
+      let baseValue = newBoard[row][col];
+      let replace = false;
 
-  const matchAndReplace = (row: number, col: number, rowInc: number, colInc: number, len: number) => {
-    let baseValue = newBoard[row][col];
-    let replace = false;
-
-    for (let i = 1; i < len; i++) {
-      if (newBoard[row + i * rowInc][col + i * colInc] !== baseValue) {
-        replace = false;
-        break;
-      }
-      replace = true;
-    }
-
-    if (replace) {
-      matches += len;
-      setAnimationBoard(prevAnimationBoard => {
-        const updatedAnimationBoard = deepCopyBoard(prevAnimationBoard);
-        for (let i = 0; i < len; i++) {
-          updatedAnimationBoard[row + i * rowInc][col + i * colInc] = matchGifIndex;
+      for (let i = 1; i < len; i++) {
+        if (newBoard[row + i * rowInc][col + i * colInc] !== baseValue) {
+          replace = false;
+          break;
         }
-        return updatedAnimationBoard;
-      });
-      setTimeout(() => {
-        const mutableBoard = deepCopyBoard(newBoard);
-        replaceCandies(mutableBoard, row, col, rowInc, colInc, len, baseValue);
-        setBoard(mutableBoard);
-        setAnimationBoard(mutableBoard);
-      }, 790); // Delay in milliseconds for animation
-    }
+        replace = true;
+      }
 
-    return replace;
-  };
+      if (replace) {
+        matches += len;
+
+        setAnimationBoard(prevAnimationBoard => {
+          const updatedAnimationBoard = deepCopyBoard(prevAnimationBoard);
+          for (let i = 0; i < len; i++) {
+            updatedAnimationBoard[row + i * rowInc][col + i * colInc] = matchGifIndex;
+          }
+          return updatedAnimationBoard;
+        });
+
+        // Play sound effect based on match type
+        if (baseValue === 0) {
+          activateSound.play();
+        } else if (baseValue === 1) {
+          fireSound.play();
+        } else if (baseValue === 2) {
+          fireSound.play();
+        } else if (baseValue === 3) {
+          fireSound.play();
+        } else if (baseValue === 4) {
+          fireSound.play();
+        } else if (baseValue === 5) {
+          fireSound.play();
+        } else if (baseValue === 6) {
+          fireSound.play();
+        }
+
+
+        setTimeout(() => {
+          const mutableBoard = deepCopyBoard(newBoard);
+          replaceCandies(mutableBoard, row, col, rowInc, colInc, len, baseValue);
+          setBoard(mutableBoard);
+          setAnimationBoard(mutableBoard);
+        }, 790); // Delay in milliseconds for animation
+      }
+
+      return replace;
+    };
 
     for (let row = 0; row < gridRows; row++) {
       for (let col = 0; col < gridCols; col++) {
@@ -241,7 +313,7 @@ export function BonkGameScreen() {
   };
 
   const handleTilePress = (rowIndex: number, colIndex: number) => {
-    if (turnCount >= 24) {
+    if (turnCount >= turnLimit) {
       console.log("Turn limit reached. No more moves allowed.");
       return;
     }
@@ -383,11 +455,11 @@ export function BonkGameScreen() {
   return (
     <div className="centered-container bg-black min-h-screen p-4">
       <div className="w-full flex flex-col items-center text-white mb-4">
-        <span className="text-lg font-bold">Turn: {turnCount}/24</span>
+        <span className="text-lg font-bold">Turn: {turnCount}/{turnLimit}</span>
         <span className="text-lg font-bold">Points: {matchCount}</span>
       </div>
       <div className="flex justify-center items-center flex-1">
-        <div className="grid grid-cols-5 gap-2">
+        <div className="grid grid-cols-7 gap-2">
           {animationBoard.map((row, rowIndex) => (
             <div key={rowIndex} className="flex flex-col">
               {row.map((candyIndex, colIndex) => (
@@ -398,12 +470,12 @@ export function BonkGameScreen() {
                     selectedTile &&
                     selectedTile.row === rowIndex &&
                     selectedTile.col === colIndex
-                      ? "opacity-50"
+                      ? "selected-tile" // Apply custom CSS class
                       : ""
                   }`}
                 >
                   <img
-                    src={candyIndex === matchGifIndex ? matchGif : candyImages[candyIndex]}
+                    src={candyIndex === matchGifIndex ? candyGifs[board[rowIndex][colIndex]] : candyImages[candyIndex]}
                     alt={`Candy ${candyIndex}`}
                     className="w-full h-full object-cover"
                   />
@@ -428,10 +500,25 @@ export function BonkGameScreen() {
         <button
           onClick={entrySubmit}
           className="btn-large bg-gold text-black rounded-full font-bold border-2 border-gold hover:bg-black hover:text-gold transition-colors duration-300"
-          disabled={turnCount < 2}
+          disabled={turnCount < turnLimit}
         >
           Submit
         </button>
+        <button
+          onClick={() => setIsMuted(!isMuted)}
+          className="btn-large bg-gray-700 text-white rounded-full font-bold border-2 border-gray-700 hover:bg-black hover:text-gray-300 transition-colors duration-300"
+        >
+          {isMuted ? 'Unmute' : 'Mute'}
+        </button>
+        <input
+          type="range"
+          min="0"
+          max="1"
+          step="0.01"
+          value={volume}
+          onChange={(e) => setVolume(parseFloat(e.target.value))}
+          className="w-full"
+        />
       </div>
       <div className="text-white mb-4" dangerouslySetInnerHTML={{ __html: transactionStatus }}></div>
       {transactionProcessing && (
